91dd3f2223d2d1644c8afb918a2c9f3b00bddffe
aariin <rahayuni.w@gmail.com>
2020-06-30 22:17:55 +0700

    - sequential program - branch - looping - array data type

---
+++ datatypes.py
@@ -0,0 +1,19 @@
+#scalar data type: simple data type
+kid1 = 'Nina'
+kid2 = 'Alex'
+kid3 = 'Laura'
+kid4 = 'Onky'
+
+print(kid1)
+print(kid2)
+print(kid3)
+print(kid4)
+
+#array data type
+kid = ['Nina', 'Alex', 'Laura', 'Onky']
+print(kid)
+kid.append('Harry')
+print(kid)
+
+#greet second kid
+print(f'Hai {kid[1]}!')
