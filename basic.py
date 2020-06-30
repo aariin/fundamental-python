830a3f9df46fb19d2516324d09f770fd26b9d54b
aariin <rahayuni.w@gmail.com>
2020-06-30 22:10:36 +0700

    - sequential program - branch - looping

---
+++ basic.py
@@ -0,0 +1,19 @@
+# basic python construction
+# sequential
+print('hello world!')
+print('by Arin')
+print('30 June 2020')
+print('_' * 5)
+
+#percabangan: Eksekusi terpilih
+short_cut = True
+if short_cut:
+    print('go ahead!')
+else:
+    print('get another way')
+
+#Perulangan
+kids = 4
+
+for kids_index in range(1,kids+1):
+    print(f'Hello kid number {kids_index}!')
