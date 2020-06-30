ea2ed6ca5a661ea06b01897cedc2436c05e528cd
aariin <rahayuni.w@gmail.com>
2020-06-30 22:04:19 +0700

    - sequential program - percabangan - perulangan

--- main.py
+++ main.py
@@ -11,3 +11,9 @@ if ingin_cepat:
     print('jalan terus aja ya!')
 else:
     print('pilih jalan lain')
+
+#Perulangan
+jumlah_anak = 4
+
+for index_anak in range(1,jumlah_anak+1):
+    print(f'Halo anak {index_anak}!')
