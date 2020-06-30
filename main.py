d8ef076ce967a6237571f316b516e552bf31278c
aariin <rahayuni.w@gmail.com>
2020-06-30 21:58:56 +0700

    - sequential program - percabangan

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ print('hello world!')
 print('by Arin')
 print('30 June 2020')
 print('_' * 5)
+
+#percabangan: Eksekusi terpilih
+ingin_cepat = True
+if ingin_cepat:
+    print('jalan terus aja ya!')
+else:
+    print('pilih jalan lain')
