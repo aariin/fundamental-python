bb16d42692fb880137a4ea1fa08be78f8bb825e3
aariin <rahayuni.w@gmail.com>
2020-06-30 22:26:36 +0700

    - sequential program - branch - looping - array data type

--- datatypes.py
+++ datatypes.py
@@ -10,10 +10,22 @@ print(kid3)
 print(kid4)
 
 #array data type
+print('\nlist data type')
 kid = ['Nina', 'Alex', 'Laura', 'Onky']
 print(kid)
 kid.append('Harry')
 print(kid)
 
 #greet second kid
+print('\ngreet second kid')
 print(f'Hai {kid[1]}!')
+
+print('\ngreet all kids')
+#special script "for" for list/array data
+for k in kid:
+    print(f'Hi {k}!')
+
+#or we can use difficult way
+print('\ngreet all kids using difficult way')
+for k in range(0, len(kid)):
+    print(f'Hi {kid[k]}!')
